9d6135167de377647a0bf9458f4d1b77b32184f6
Mike Parcewski <mike.parcewski@gmail.com>
2022-09-11 18:01:28 -0400

    move to word cloud

--- src/Components/Skills.tsx
+++ src/Components/Skills.tsx
@@ -1,77 +1,60 @@
-import React from 'react';
+import React, { useEffect, useRef } from "react";
 import SimpleHeader, { SimpleHeaderParams } from './SimpleHeader';
-import { Box, Chip } from '@mui/material';
+import { Box } from '@mui/material';
 import { SimpleResume, Skill } from '../Objects/SimpleResume';
-
-const LEVELS : string[] = [
-  "beginner", "intermediate", "advanced", "master"
-];
-
-const COLORS = [
-  'secondary', 'primary', 'success', 'warning', undefined
-] as const;
+import WordCloud from "wordcloud";
 
 
 const Skills = (props: SimpleResume) => {
 
-    let headerParams : Partial<SimpleHeaderParams> = {
-      title: props.skillsPageTitle,
-      conf: props
-    };
+  let headerParams : Partial<SimpleHeaderParams> = {
+    title: props.skillsPageTitle,
+    conf: props
+  };
 
-    // remove to retain order
-    let shuffled : Skill[] = props.resume?.skills
-      .map(value => ({ value, sort: Math.random() }))
-      .sort((a, b) => a.sort - b.sort)
-      .map(({ value }) => value) as Skill[];
+  const [width] = React.useState(window.innerWidth*.66);
+  const [height] = React.useState(window.innerHeight*.66);
 
-    return (
+  const canvasRef = useRef(null);
 
-      <>
+  useEffect(() => {
 
-        <SimpleHeader {...headerParams} />
+    WordCloud(canvasRef.current, {
+      list: Array.from(
+        props.resume!.skills, 
+        (skill : Skill) => [ skill.name, skill.level ]
+      ),
+      shape: "circle",
+      minRotation: 0,
+      maxRotation: 0,
+      shrinkToFit: true,
+      minSize: 2
+    });
 
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            my: "2vmax",
-          }}
-        >
+  }, [props.resume]);
 
-          <Box
-           sx={{
-             maxWidth: { xs: '95%', sm: '80%' },
-           }}
-          >
+  return (
 
-            {shuffled && shuffled.map((skill : Skill) => (
+    <>
 
-              <Chip 
-                key={skill.name.replace(' ', '_') + "-chip"}
-                label={skill.name} 
-                color={COLORS[getColorIdx(skill.level)]} 
-                size="medium"
-                sx={{
-                  mx: "1vmax",
-                  my: "1vmax"
-                }}
-              />
-            ))}
+      <SimpleHeader {...headerParams} />
 
-          </Box>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          my: "2vmax",
+        }}
+      >
 
-        </Box>
+        <canvas ref={canvasRef} width={width} height={height} />
+ 
+      </Box>
 
 
-      </>
+    </>
 
-    )
+  )
 }
 export default Skills;
-
-function getColorIdx(type : string) : number {
-  if (type === "") return Math.floor(Math.random() * (4 - 1 + 1));
-  return LEVELS.indexOf(type.toLowerCase());
-}
